169483deee1e6c6b2143591aba454702be6e8edc
Bernhard Häussermann <bernhardgh@icloud.com>
2026-04-24 20:33:39 +0200

    Web build script: Add commands for installing Go before build

--- build-web.sh
+++
@@ -1,4 +0,0 @@
-rm -rf bin/web
-mkdir bin/web
-env GOOS=js GOARCH=wasm go build -o bin/web/astronaut.wasm github.com/bhaeussermann/ebitengine-astronaut/src
-cp web/*.* bin/web
---
+++ vercel-build-web.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# Install Go
+GO_VERSION="1.26.1"
+curl -sL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" | tar -xz -C $HOME
+export PATH="$HOME/go/bin:$PATH"
+
+# Build WASM
+rm -rf bin/web
+mkdir bin/web
+env GOOS=js GOARCH=wasm go build -o bin/web/astronaut.wasm github.com/bhaeussermann/ebitengine-astronaut/src
+cp web/*.* bin/web
