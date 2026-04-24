55f8bed16b7019ab30a111cbaaea4f2cdb889abd
Bernhard Häussermann <bernhardgh@icloud.com>
2026-04-24 20:39:44 +0200

    Fix web build script folder creation

--- vercel-build-web.sh
+++ vercel-build-web.sh
@@ -8,6 +8,6 @@ export PATH="$HOME/go/bin:$PATH"
 
 # Build WASM
 rm -rf bin/web
-mkdir bin/web
+mkdir -p bin/web
 env GOOS=js GOARCH=wasm go build -o bin/web/astronaut.wasm github.com/bhaeussermann/ebitengine-astronaut/src
 cp web/*.* bin/web
