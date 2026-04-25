863b44c800ff7bf2456c2a50f8d874c6d01a035e
Bernhard Häussermann <bernhardgh@icloud.com>
2026-04-25 15:03:14 +0200

    Vercel build script: Fix X11 header not found

--- vercel-build-web.sh
+++ vercel-build-web.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install X11 headers for Ebitengine
-apt-get install -y libx11-dev libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev libgl1-mesa-dev
-
 # Install Go
 GO_VERSION="1.26.1"
 curl -sL "https://go.dev/dl/go${GO_VERSION}.linux-amd64.tar.gz" | tar -xz -C $HOME
@@ -13,9 +10,8 @@ export PATH="$HOME/golang/bin:$PATH"
 # Build WASM
 rm -rf bin/web
 mkdir -p bin/web
-env GOOS=js GOARCH=wasm
 cd src
-go build -o ../bin/web/astronaut.wasm github.com/bhaeussermann/ebitengine-astronaut
+env GOOS=js GOARCH=wasm go build -o ../bin/web/astronaut.wasm github.com/bhaeussermann/ebitengine-astronaut
 cd ..
 cp web/*.* bin/web
 mkdir bin/web/images
